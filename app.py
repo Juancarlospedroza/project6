7e1939f1d1adc5fe5bfc11e2afab0490903cdc9e
Juan Carlos Pedroza <pedrozajuancarlos@gmail.com>
2024-11-04 17:16:53 -0600

    App de autos

--- app.py
+++ app.py
@@ -3,19 +3,15 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-st.header('Esto está muy macabro')
-st.write('presiona el botón si te atreves')
+st.header('Autos a la venta')
+st.write('Aquí puedes encontrar los autos que tenemos disponibles')
 
 # Data frame limpio para usarse
 car_data = pd.read_csv('notebooks/vehicles_clean_df.csv')
 
-hist_button = st.button('Construir histograma') # crear un botón
+hist_button = st.button('Precios de nuestros autos') # crear un botón
 if hist_button: # al hacer clic en el botón
-    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
-    fig = px.histogram(car_data, x="price")
+    st.write('Distribución de los precios')
+    fig = px.histogram(car_data, nbins=500, x="price")
+    fig.update_xaxes(range=[1000, 50000])
     st.plotly_chart(fig, use_container_width=True)
-
-
-build_histogram = st.checkbox('dale clic')
-if build_histogram:
-    st.write('Tu suegra está atrás de ti D:')
