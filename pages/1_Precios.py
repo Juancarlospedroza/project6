a254a9291b4e2b94aa8bd01c796ba5cfcbfe754d
Juan Carlos Pedroza <pedrozajuancarlos@gmail.com>
2024-11-04 20:03:03 -0600

    Corrección de los df en precios

--- pages/1_Precios.py
+++ pages/1_Precios.py
@@ -12,6 +12,8 @@ opcion = st.selectbox(
 )
 
 df = pd.read_csv('notebooks/vehicles_clean_df.csv')
+df = df.iloc[:, 0:8]
+df = df[df["price"] > 2500]
 
 if opcion == "Menos de 10,000 DLS":
     st.dataframe(df[df["price"] < 10000].sort_values(by="price").reset_index(drop=True))
@@ -19,6 +21,11 @@ if opcion == "Menos de 10,000 DLS":
 if opcion == "De 10,000 a menos de 20,000 DLS":
     st.dataframe(df[(df["price"] >= 10000) & (df["price"] < 20000)].sort_values(by="price").reset_index(drop=True))
 
+if opcion == "De 20,000 a menos de 30,000 DLS":
+    st.dataframe(df[(df["price"] >= 20000) & (df["price"] < 30000)].sort_values(by="price").reset_index(drop=True))
+
+if opcion == "De 30,000 a menos de 40,000 DLS":
+    st.dataframe(df[(df["price"] >= 30000) & (df["price"] < 40000)].sort_values(by="price").reset_index(drop=True))
 
 if opcion == "Más de 40,000 DLS":
     st.dataframe(df[df["price"] > 40000].sort_values(by="price").reset_index(drop=True))
--- pages/2_Dataframe completo.py
+++ pages/2_Dataframe completo.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 
 st.header('Data frame')
-st.write('Este es el dataframe ya limpio del cual se generan las gráficas que se muestran en los demás apartados.')
+st.write('Este es el dataframe completo de los vehículos.')
 
 data = pd.read_csv('notebooks/vehicles_clean_df.csv')
 st.write(data)
